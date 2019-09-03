10c9f44f851dc8c802a3e3658df61fe61a73097b
saddique <saddique_uet@yahoo.com>
2019-09-03 23:43:45 +0400

    first commit

---
+++ Deploy-keyvault.ps1
@@ -0,0 +1,31 @@
+﻿{
+$resourceGroupName = 'SantiaDemo'
+$resourceGroupLocation = 'Southeast Asia'
+$resoruceDeploymentName= "santiakeyvault"
+$templatePath = $env:SystemDrive + '\' + 'santia\SantiaKeyvault'
+$templateName= 'azuredeploy.json'
+$template = $templatePath + '\' + $templateName
+}
+
+
+# Creating a new Resource Group
+
+{
+New-AzureRmResourceGroup `
+  -Location $resourceGroupLocation `
+  -Name $resourceGroupName `
+  -verbose -Force
+}
+
+# Creation of Virtual Network
+
+{
+ New-AzureRmResourceGroupDeployment `
+  -Name $resoruceDeploymentName `
+  -ResourceGroupName $resourceGroupName `
+  -TemplateFile $template `
+  -verbose -Force
+
+}
+
+
---
+++ azuresignedcertificate.ps1
@@ -0,0 +1,24 @@
+﻿
+
+$policy = New-AzureKeyVaultCertificatePolicy `
+    -SubjectName "CN=www.contoso.com" `
+    -SecretContentType "application/x-pkcs12" `
+    -IssuerName Self `
+    -ValidityInMonths 12
+
+Add-AzureKeyVaultCertificate `
+    -VaultName $resoruceDeploymentName `
+    -Name "mycert" `
+    -CertificatePolicy $policy 
+
+
+
+
+
+$certURL=(Get-AzureKeyVaultSecret -VaultName $keyvaultName -Name "mycert").id
+
+$vm=Get-AzVM -ResourceGroupName $resourceGroup -Name "myVM"
+$vaultId=(Get-AzKeyVault -ResourceGroupName $resourceGroup -VaultName $keyVaultName).ResourceId
+$vm = Add-AzVMSecret -VM $vm -SourceVaultId $vaultId -CertificateStore "My" -CertificateUrl $certURL
+
+Update-AzVM -ResourceGroupName $resourceGroup -VM $vm
